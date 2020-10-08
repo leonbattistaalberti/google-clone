5a3c5bbcb46e79a80dc64b00c85829789ff7e833
leon_battist_alberti <leonbattista189@gmail.com>
2020-10-08 01:52:15 +0100

    Add about, store, gmail, images links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,20 @@
 import React from "react";
 import "./Home.css";
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <div className="home">
       <h1> This is the Home Page</h1>
       <div className="home__header">
         <div className="home__headerLeft">
-          {/* Link */}
-          {/* Link */}
+          <Link to="about">About</Link>
+          <Link to="store">Store</Link>
         </div>
         <div className="home__headerRight">
-          {/* Link */}
-          {/* Link */}
+          <Link to="gmail">Gmail</Link>
+          <Link to="images">Images</Link>
+
           {/* Icon */}
           {/* Avatar */}
         </div>
