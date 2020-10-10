ed8f928070667bbda670b0640e4479ecdd69d7df
leon_battist_alberti <leonbattista189@gmail.com>
2020-10-10 20:19:00 +0100

    Add dotenv

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,9 +1,21 @@
 import React from "react";
+import "./SearchPage.css";
+import { useStateValue } from "../StateProvider";
 
+// https://developers.google.com/custom-search/v1/using_rest
+
+// search engine id: 05cd5cd79fdcb94b5
+
+const API_KEY = process.env.API_KEY;
+console.log(process.env);
 const SearchPage = () => {
+  const [state, dispatch] = useStateValue();
+
   return (
-    <div>
-      <h1>This is the search page</h1>
+    <div className="searchPage">
+      <div className="searchPage__header">
+        <h1>{state.term}</h1>
+      </div>
     </div>
   );
 };
---
+++ src/hooks/useGoogleSearch.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+
+const API_KEY = process.env.API_KEY;
+
+const useGoogleSearch = ({ term }) => {
+  return <div></div>;
+};
+
+export default useGoogleSearch;
