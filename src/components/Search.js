5faddbdd56104a1c2ec1f50d2111fbfb6b47b4ba
leon_battist_alberti <leonbattista189@gmail.com>
2020-10-10 16:06:34 +0100

    Add dispatch to Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,13 +3,23 @@ import "./Search.css";
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
 import { Button } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import { useStateValue } from "../StateProvider";
+import { actionTypes } from "../reducer";
 
 const Search = ({ hideButtons }) => {
+  const [{}, dispatch] = useStateValue();
   const [input, setInput] = useState("");
+  const history = useHistory();
 
   const search = (evt) => {
     evt.preventDefault();
-    // TODO:
+    history.push("/search");
+
+    dispatch({
+      type: actionTypes.SET_SEARCH_TERM,
+      term: input,
+    });
   };
 
   return (
