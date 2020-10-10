b1d92ce6f8bff22c830369f411abffb3fbcc63d4
leon_battist_alberti <leonbattista189@gmail.com>
2020-10-10 21:31:59 +0100

    Add searchPage logo

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -3,6 +3,7 @@ import "./SearchPage.css";
 import { useStateValue } from "../StateProvider";
 import useGoogleSearch from "../hooks/useGoogleSearch";
 import response from "../utils/response";
+import { Link } from "react-router-dom";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 // https://developers.google.com/custom-search/v1/using_rest
@@ -15,7 +16,13 @@ const SearchPage = () => {
   return (
     <div className="searchPage">
       <div className="searchPage__header">
-        <h1>{state.term}</h1>
+        <Link to="/">
+          <img
+            className="searchPage__logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1024px-Google_2015_logo.svg.png"
+            alt="google logo"
+          />
+        </Link>
       </div>
     </div>
   );
