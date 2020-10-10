f9a7257be231561238d13f37aa454ff1d97ad66c
leon_battist_alberti <leonbattista189@gmail.com>
2020-10-10 01:22:40 +0100

    Add search bar buttons

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Search.css";
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
+import { Button } from "@material-ui/core";
 
 const Search = () => {
   return (
@@ -10,6 +11,10 @@ const Search = () => {
         <SearchIcon className="search__inputIcon" />
         <input />
         <MicIcon />
+      </div>
+      <div className="search__buttons">
+        <Button variant="outlined"> Google Search </Button>
+        <Button variant="outlined"> I am Feeling Lucky </Button>
       </div>
     </div>
   );
