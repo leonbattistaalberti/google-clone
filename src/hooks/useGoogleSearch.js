2085d4e650dbdae0ed9a03e54e3b0818be8a967d
leon_battist_alberti <leonbattista189@gmail.com>
2020-10-10 20:32:08 +0100

    Complete useGoogleSearch hook

--- src/hooks/useGoogleSearch.js
+++ src/hooks/useGoogleSearch.js
@@ -4,7 +4,19 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 const CONTEXT_KEY = "05cd5cd79fdcb94b5";
 
 const useGoogleSearch = ({ term }) => {
-  return <div></div>;
+  const [data, setData] = useGoogleSearch(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+      );
+      const result = await response.json();
+      setData(result);
+    };
+    fetchData();
+  }, [term]);
+
+  return { data };
 };
 
 export default useGoogleSearch;
